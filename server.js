c35595d305e76b92d4333debbe924e2f5d04b4ab
Rishit <51650467+rishittripathi-therocking@users.noreply.github.com>
2022-08-13 20:18:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static('./dist/movie-list'));
-
-app.get('/*',(req,res) => res.sendFile('index.html', {root: 'dist/movie-list/'}));
+app.use(express.static(__dirname + '/dist'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 
 app.listen(process.env.PORT || 8080);
